a3fd1818a4911162861615ca230895781e751461
Afif Elghraoui <aelghraoui@sdsu.edu>
2016-05-23 17:18:19 -0700

    Initial commit of blasr.mk

---
+++ blasr.mk
@@ -0,0 +1,71 @@
+
+BLASR ?= blasr
+BLASR_NPROC ?= $(NPROC)
+BLASR_REF ?= $(REF)
+
+ifneq ($(BLASR_NPROC),)
+BLASRFLAGS += -nproc $(BLASR_NPROC)
+endif
+
+ifdef BLASR_CONCORDANT
+BLASRFLAGS += -concordant
+endif
+
+ifdef BLASR_IGNOREREGIONS
+BLASRFLAGS += -ignoreRegions
+endif
+
+ifdef BLASR_USEQUALITY
+BLASRFLAGS += -useQuality
+endif
+
+ifdef BLASR_RANDOMSEED
+BLASRFLAGS += -randomSeed $(BLASR_RANDOMSEED)
+endif
+
+ifdef BLASR_BESTN
+BLASRFLAGS += -bestn $(BLASR_BESTN)
+endif
+
+ifdef BLASR_MINPCTIDENTITY
+BLASRFLAGS += -minPctIdentity $(BLASR_MINPCTIDENTITY)
+endif
+
+ifdef BLASR_MINMATCH
+BLASRFLAGS += -minMatch $(BLASR_MINMATCH)
+endif
+
+ifdef BLASR_PLACEREPEATSRANDOMLY
+BLASRFLAGS += -placeRepeatsRandomly
+endif
+
+ifdef BLASR_HITPOLICY
+BLASRFLAGS += -hitPolicy $(BLASR_HITPOLICY)
+endif
+
+ifdef BLASR_MINPCTACCURACY
+BLASRFLAGS += -minPctAccuracy $(BLASR_MINPCTACCURACY)
+endif
+
+ifdef BLASR_MINSUBREADLENGTH
+BLASRFLAGS += -minSubreadLength $(BLASR_MINSUBREADLENGTH)
+endif
+
+ifdef BLASR_MINREADLENGTH
+BLASRFLAGS += -minReadLength $(BLASR_MINREADLENGTH)
+endif
+
+# These options require or create additional files
+
+ifdef BLASR_REGIONTABLE
+BLASRFLAGS += -regionTable $(BLASR_REGIONTABLE)
+%.sam: $(BLASR_REGIONTABLE)
+endif
+
+ifdef BLASR_UNALIGNEDFILE
+BLASRFLAGS += -unaligned $(BLASR_UNALIGNEDFILE)
+endif
+
+
+%.sam: $(BLASR_INPUT) $(BLASR_REF)
+	$(BLASR) $< $(word 2,$^) -sam -out $@ $(BLASRFLAGS)
