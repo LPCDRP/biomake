7883c02df83d18e65ea20118c8862aa117004d55
Afif Elghraoui <aelghraoui@sdsu.edu>
2017-02-03 12:20:45 -0800

    Add pbhoney.mk

---
+++ include/pbhoney.mk
@@ -0,0 +1,36 @@
+
+include samtools.mk
+
+# Set the inputReads and reference -- change at your leisure
+# inputReads ?= corrected.fastq
+# reference ?= final.fasta
+
+NPROC ?= $(shell nproc)
+
+# Buffer around breaks reads must fall within to become clustered
+ifdef TAILS_BUFFER
+TAILS_FLAGS += --buffer $(TAILS_BUFFER)
+endif
+
+# Minimum number of reads
+ifdef TAILS_MINBREADS
+TAILS_FLAGS += --minBreads $(TAILS_MINBREADS)
+endif
+
+# Minimum number of unique ZMWs
+ifdef TAILS_MINZMWS
+TAILS_FLAGS += --minZMWs $(TAILS_MINZMWS)
+endif
+
+# Minimum mapping quality of a read and its tail to consider
+ifdef TAILS_MINMAPQ
+TAILSFLAGS += --minMapq $(TAILS_MINMAPQ)
+endif
+
+PIEFLAGS += --nproc $(NPROC)
+
+%.hon.tails: $(addprefix %.tails,.sort.bam .sort.bam.bai)
+	Honey.py tails $(TAILSFLAGS) $< -o $@  2>&1 | tee tails.log
+
+%.sam: $(inputReads) $(REF)
+	Honey.py pie $(PIEFLAGS) $< $(word 2,$^) -o $@ 2>&1 | tee pie.log
