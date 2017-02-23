e551aadd2d3c143b57f3c443897de2765576e434
Afif Elghraoui <aelghraoui@sdsu.edu>
2017-02-23 10:48:18 -0800

    Standardize bwa.mk

--- include/bwa.mk
+++ include/bwa.mk
@@ -3,37 +3,21 @@
 # bwa makefile rules
 # 
 
-MAKEDIR = $(dir $(lastword $(MAKEFILE_LIST)))
-ifeq ($(findstring ngsvars.mk,$(MAKEFILE_LIST)),)
-include $(MAKEDIR)ngsvars.mk
-endif
-ifeq ($(findstring samtools.mk,$(MAKEFILE_LIST)),)
-include $(MAKEDIR)samtools.mk
-endif
-
-# bwa
 ifndef BWA
 BWA=bwa
 endif
-# -M: mark shorter split reads as secondary (compatibility with GATK and picard)
-ifndef BWA_OPTIONS
-BWA_OPTIONS=-t $(NPROC) -M
+
+ifdef NPROC
+BWAFLAGS += -t $(NPROC)
 endif
 
-%.bam: %$(READ1_LABEL).fastq.gz %$(READ2_LABEL).fastq.gz
-	$(BWA) mem $(BWA_OPTIONS) $(REFERENCE) $^ | $(SAMTOOLS) view -Sbh - > $@.tmp && mv $@.tmp $@
+ifdef BWA_MARKSECONDARY
+BWA_OPTIONS += -M
+endif
 
-##############################
-# settings
-##############################
-.PHONY: bwa-settings bwa-header
+ifndef FASTQDIR
+$(error FASTQDIR must be defined as the path to the directory containing the input fastq.gz files)
+endif
 
-print-%:
-	@echo '$*=$($*)'
-
-bwa-header:
-	@echo -e "\nbwa.mk options"
-	@echo "===================="
-
-
-bwa-settings: bwa-header print-BWA print-NPROC print-BWA_OPTIONS print-REFERENCE
+%.bam: $(REFERENCE) $(FASTQDIR)/%_*.fastq.gz
+	$(BWA) mem $(BWAFLAGS) $< $(filter-out $<, $^) | samtools view -Sbh - > $@.tmp && mv $@.tmp $@
